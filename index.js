9d9a6038d65cf1750c20c4165d58a58b6e0c7c0b
独梦寒风 <1254442318@qq.com>
2022-09-07 18:04:57 +0800

    代码提交

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+name 发布的包名，默认是上级文件夹名。不得与现在npm中的包名重复。包名不能有大写字母/空格/下滑线!
+version 版本,默认是1.0.0
+description 描述
+main 入口文件
+scripts：包含各种脚本执行命令  类似于 start  dev build
+test：测试命令。
+author：写自己的账号名
+license：开源协议
+*/
+console.log('1111')
